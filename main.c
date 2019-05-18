5bc3a6c2d8c23fc0f9f2b3f6acb7e46c874aec1b
Anselmo Battisti <anselmobattisti@gmail.com>
2019-05-18 20:39:13 -0300

    Exemplo de navegação
    Exemplo de navegação

--- main.c
+++ main.c
@@ -12,6 +12,10 @@ int main() {
   big = insere_filho_quadrado(big,3,3);
   big = insere_filho_quadrado(big,4,4);
 
+  // inserer um quadrado como filho do segundo filho de big
+  insere_filho_quadrado(big->f->i,5,5);
+  insere_filho_quadrado(big->f->i,6,6);
+
   imprimir_status_filhos(big);
 
   // big = insere_filho_quadrado(big,20);

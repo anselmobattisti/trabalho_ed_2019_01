dcf64400ba64ee26405c771a4dff8dcc37e38d94
Anselmo Battisti <anselmobattisti@gmail.com>
2019-05-31 15:50:54 -0300

    pai como mesmo pai corrigido
    pai como mesmo pai corrigido

--- main.c
+++ main.c
@@ -398,7 +398,7 @@ void menuMover(){
                       printf(COLOR_RED"Código novo pai não pode ser ele mesmo."COLOR_RESET);
                     }
                   }
-                  if (cod_novo_pai == cod) {
+                  if (cod_novo_pai == cod || pai_aux->cod == cod_novo_pai) {
                       printf(COLOR_RED"Código novo pai não pode ser ele mesmo 2."COLOR_RESET);
                   } else {
                     if (cod_novo_pai != -1) {
